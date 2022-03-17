4a2cfe5b434b0bdd7e464e908a28f84ca7d9db08
temitope <temitope@elejant.com>
2022-03-17 15:43:02 +0100

    Updates

---
+++ pages/onboarding/verify-email/index.tsx
@@ -0,0 +1,31 @@
+import type { NextPage } from 'next';
+
+export async function getStaticProps() {
+    return { props: { blue: "true" } };
+}
+const VerifyEmail: NextPage = (props) => {
+    return (
+        <div className='page'>
+            <div className='container'>
+                <div className='page-content'>
+                    <img className='logo' src={'/logo.svg'} width='195px' height='45px'></img>
+                    <form>
+                        <div className='headline'>Verify you Email</div>
+                        <div className='form-content'>
+                            <img src='/icons/verify-email.svg' width='80' className="d-block mx-auto"></img>
+
+                            <div className='mt-4'>
+                                We sent a verification email to <b>akinolayemi100@gmail.com</b>. Click the link inside to get started!
+                            </div>
+                            <div className='text-center mt-4'>
+                                <button className='btn btn-primary d-inline-block'>Resend Email</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default VerifyEmail
---
+++ pages/signup/index.tsx
@@ -0,0 +1,181 @@
+import { MenuItem, Select, TextField } from '@mui/material';
+import type { NextPage } from 'next'
+import Link from 'next/link';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import DatePicker from '@mui/lab/DatePicker';
+import React from 'react';
+import InputAdornment from '@mui/material/InputAdornment';
+import IconButton from '@mui/material/IconButton';
+import { RemoveRedEye } from '@mui/icons-material';
+
+export async function getStaticProps() {
+    return {
+        props: { blue: "true" }
+    };
+}
+
+const Signup: NextPage = (props) => {
+    const [value, setValue] = React.useState(null);
+    const [step, setStep] = React.useState(1);
+    return (
+        <div className='page'>
+            <div className='container'>
+                <div className='page-content'>
+                    <img className='logo' src={'/logo.svg'} width='195px' height='45px'></img>
+                    <form>
+                        <div className='headline'>Create your account</div>
+                        <div className='form-content'>
+                            {step == 1 &&
+                                <>
+                                    <div className='mt-3'>
+                                        <label className='mb-2'>First name*</label>
+                                        <TextField
+                                            {...props} variant="standard"
+                                            placeholder='First name'
+                                            fullWidth
+                                            InputProps={{
+                                                disableUnderline: true
+                                            }}
+                                        />
+                                    </div>
+                                    <div className='mt-3'>
+                                        <label className='mb-2'>Last name*</label>
+                                        <TextField
+                                            {...props} variant="standard"
+                                            placeholder='Last name'
+                                            fullWidth
+                                            InputProps={{
+                                                disableUnderline: true
+                                            }}
+                                        />
+                                    </div>
+                                    <div className='mt-4'>
+                                        <label className='mb-2'>Gender*</label>
+                                        <Select disableUnderline
+                                            placeholder='Gender' displayEmpty
+                                            id="demo-simple-select" className='form-control' variant='standard'
+                                        >
+                                            <MenuItem>
+                                                Select Gender
+                                            </MenuItem>
+                                            <MenuItem value='Male'>Male</MenuItem>
+                                            <MenuItem value='Female'>Female</MenuItem>
+                                        </Select>
+                                    </div>
+                                    <div className='mt-4'>
+                                        <label className='mb-2'>Date of Birth*</label>
+                                        <LocalizationProvider dateAdapter={AdapterDateFns}>
+                                            <DatePicker InputProps={{
+                                                disableUnderline: true,
+                                            }}
+                                                value={value}
+                                                onChange={(newValue) => {
+                                                    setValue(newValue);
+                                                }}
+                                                renderInput={(params) => <TextField variant='standard' fullWidth placeholder='Date of birth (DD/MM/YYY)' {...params} />} />
+                                        </LocalizationProvider>
+                                    </div>
+                                    <div className='mt-4'>
+                                        <button onClick={() => setStep(2)} className='btn btn-primary'>Next</button>
+                                    </div>
+                                </>
+                            }
+
+                            {step == 2 &&
+                                <>
+                                    <div className='mt-4'>
+                                        <label className='mb-2'>Email address*</label>
+                                        <TextField
+                                            {...props} variant="standard"
+                                            placeholder='Email address'
+                                            fullWidth
+                                            InputProps={{
+                                                disableUnderline: true
+                                            }}
+                                        />
+                                    </div>
+                                    <div className='mt-4'>
+                                        <label className='mb-2'>Phone Number*</label>
+                                        <TextField
+                                            {...props} variant="standard"
+                                            placeholder='+234'
+                                            fullWidth
+                                            InputProps={{
+                                                disableUnderline: true
+                                            }}
+                                        />
+                                    </div>
+                                    <div className='mt-4'>
+                                        <label className='mb-2'>Password*</label>
+                                        <TextField
+                                            {...props} variant="standard"
+                                            placeholder='Password'
+                                            fullWidth
+                                            InputProps={{
+                                                disableUnderline: true,
+                                                endAdornment: (
+                                                    <InputAdornment position="end">
+                                                        <IconButton edge="end" >
+                                                            <RemoveRedEye className='grey-icon' />
+                                                        </IconButton>
+                                                    </InputAdornment>
+                                                ),
+                                            }}
+                                        />
+                                    </div>
+                                    <div className='mt-4'>
+                                        <label className='mb-2'>Confirm Password*</label>
+                                        <TextField
+                                            {...props} variant="standard"
+                                            placeholder='Confirm Password'
+                                            fullWidth
+                                            InputProps={{
+                                                disableUnderline: true,
+                                                endAdornment: (
+                                                    <InputAdornment position="end">
+                                                        <IconButton edge="end" >
+                                                            <RemoveRedEye className='grey-icon' />
+                                                        </IconButton>
+                                                    </InputAdornment>
+                                                ),
+                                            }}
+                                        />
+                                    </div>
+                                    <div className='mt-4'>
+                                        <label className='mb-2'>Got referral code?</label>
+                                        <TextField
+                                            {...props} variant="standard"
+                                            placeholder='Enter referral code (optional)'
+                                            fullWidth
+                                            InputProps={{
+                                                disableUnderline: true
+                                            }}
+                                        />
+                                    </div>
+                                    <div className='mt-5'>
+                                        <h6>Terms & Conditions</h6>
+                                        <div className='mt-2'><input type='checkbox'></input> I agree with the terms & conditions</div>
+                                    </div>
+                                    <div className='mt-4'>
+                                        <Link href='/onboarding/set-pin'>
+                                            <button className='btn btn-primary'>Sign Up</button>
+                                        </Link>
+
+                                    </div>
+                                </>
+                            }
+
+                            <div className='mt-4'>
+                                <span>Already have an account?</span> <Link href='/signin'>Sign in now</Link>
+                            </div>
+
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Signup
