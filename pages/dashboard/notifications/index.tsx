f85d009b5998d1b7b06eea516ae73aefda151b81
temitope <temitope@elejant.com>
2022-08-19 15:38:41 +0100

    Update

--- pages/dashboard/notifications/index.tsx
+++ pages/dashboard/notifications/index.tsx
@@ -2,6 +2,10 @@ import { NextPage } from "next";
 import NotificationCard from "../../../components/cards/notification/notification";
 import DashboardHeader from "../../../components/dashboard-header/dashboard-header";
 import DashboardSidebar from "../../../components/dashboard-sidebar/dashboard-sidebar";
+import Pagination from '@mui/material/Pagination';
+import { PaginationItem } from "@mui/material";
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
 const Notifications: NextPage = () => {
     const data = [
@@ -28,7 +32,22 @@ const Notifications: NextPage = () => {
             timestamp: "12th Feb, 2022",
             amount: "NGN 100,000",
             type: 'deposit'
-        }
+        },{
+            id: '4',
+            title: "BTC Sold",
+            description: "Sold 0.07027 BTC for NGN 1,708,624.00	",
+            timestamp: "12th Feb, 2022",
+            amount: "0.07027 BTC",
+            type: 'debit'
+        },
+        {
+            id: '5',
+            title: 'BTC received',
+            description: 'Sold 0.07027 BTC for NGN 1,708,624.00',
+            timestamp: "12th Feb, 2022",
+            amount: "0.07724 BTC",
+            type: 'credit'
+        },
     ];
     return (
         <div className="dashboard">
@@ -41,7 +60,12 @@ const Notifications: NextPage = () => {
                     {data.map((item) => (
                         <NotificationCard type={item.type} key={item.id} title={item.title} description={item.description} timestamp={item.timestamp} amount={item.amount}></NotificationCard>
                     ))}
+                    <div className="pagination">
+                        <Pagination count={5} shape="rounded"
+                        />
+                    </div>
                 </div>
+
             </div>
         </div>
     )
